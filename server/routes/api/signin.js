2e0e78f98f3514beda91574649aaa0a29ae1a210
Jordan Mullen <jordan.mullen@hotmail.com>
2019-06-04 02:06:33 -0600

    Initial Commit from forked boilerplate

---
+++ server/routes/api/signin.js
@@ -0,0 +1,145 @@
+const User = require('../../models/User');
+const UserSession = require('../../models/UserSession');
+
+
+module.exports = (app) => {
+  /*
+   * Sign up
+   */
+  app.post('/api/account/signup', (req, res, next) => {
+    const { body } = req;
+    const {
+      password
+    } = body;
+    let {
+      email
+    } = body;
+
+    if (!email) {
+      return res.send({
+        success: false,
+        message: 'Error: Email cannot be blank.'
+      });
+    }
+    if (!password) {
+      return res.send({
+        success: false,
+        message: 'Error: Password cannot be blank.'
+      });
+    }
+
+    email = email.toLowerCase();
+    email = email.trim();
+
+    // Steps:
+    // 1. Verify email doesn't exist
+    // 2. Save
+    User.find({
+      email: email
+    }, (err, previousUsers) => {
+      if (err) {
+        return res.send({
+          success: false,
+          message: 'Error: Server error'
+        });
+      } else if (previousUsers.length > 0) {
+        return res.send({
+          success: false,
+          message: 'Error: Account already exist.'
+        });
+      }
+
+      // Save the new user
+      const newUser = new User();
+
+      newUser.email = email;
+      newUser.password = newUser.generateHash(password);
+      newUser.save((err, user) => {
+        if (err) {
+          return res.send({
+            success: false,
+            message: 'Error: Server error'
+          });
+        }
+        return res.send({
+          success: true,
+          message: 'Signed up'
+        });
+      });
+    });
+
+  });
+
+  app.post('/api/account/signin', (req, res, next) => {
+    const { body } = req;
+    const {
+      password
+    } = body;
+    let {
+      email
+    } = body;
+
+
+    if (!email) {
+      return res.send({
+        success: false,
+        message: 'Error: Email cannot be blank.'
+      });
+    }
+    if (!password) {
+      return res.send({
+        success: false,
+        message: 'Error: Password cannot be blank.'
+      });
+    }
+
+    email = email.toLowerCase();
+    email = email.trim();
+
+    User.find({
+      email: email
+    }, (err, users) => {
+      if (err) {
+        console.log('err 2:', err);
+        return res.send({
+          success: false,
+          message: 'Error: server error'
+        });
+      }
+      if (users.length != 1) {
+        return res.send({
+          success: false,
+          message: 'Error: Invalid'
+        });
+      }
+
+      const user = users[0];
+      if (!user.validPassword(password)) {
+        return res.send({
+          success: false,
+          message: 'Error: Invalid'
+        });
+      }
+
+      // Otherwise correct user
+      const userSession = new UserSession();
+      userSession.userId = user._id;
+      userSession.save((err, doc) => {
+        if (err) {
+          console.log(err);
+          return res.send({
+            success: false,
+            message: 'Error: server error'
+          });
+        }
+
+        return res.send({
+          success: true,
+          message: 'Valid sign in',
+          token: doc._id
+        });
+      });
+    });
+  });
+
+}
