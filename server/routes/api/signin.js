e633844bfe3d50fdeee7cab70a19d8d0a0dacbae
Jordan Mullen <jordan.mullen@hotmail.com>
2019-06-18 17:43:30 -0600

    connecting to UI

--- server/routes/api/signin.js
+++ server/routes/api/signin.js
@@ -1,6 +1,4 @@
 const User = require('../../models/User');
-const UserSession = require('../../models/UserSession');
-
 
 module.exports = (app) => {
   /*
@@ -14,7 +12,7 @@ module.exports = (app) => {
     let {
       email
     } = body;
-
+    
     if (!email) {
       return res.send({
         success: false,
@@ -27,10 +25,8 @@ module.exports = (app) => {
         message: 'Error: Password cannot be blank.'
       });
     }
-
     email = email.toLowerCase();
     email = email.trim();
-
     // Steps:
     // 1. Verify email doesn't exist
     // 2. Save
@@ -48,10 +44,8 @@ module.exports = (app) => {
           message: 'Error: Account already exist.'
         });
       }
-
       // Save the new user
       const newUser = new User();
-
       newUser.email = email;
       newUser.password = newUser.generateHash(password);
       newUser.save((err, user) => {
@@ -67,79 +61,5 @@ module.exports = (app) => {
         });
       });
     });
-
-  });
-
-  app.post('/api/account/signin', (req, res, next) => {
-    const { body } = req;
-    const {
-      password
-    } = body;
-    let {
-      email
-    } = body;
-
-
-    if (!email) {
-      return res.send({
-        success: false,
-        message: 'Error: Email cannot be blank.'
-      });
-    }
-    if (!password) {
-      return res.send({
-        success: false,
-        message: 'Error: Password cannot be blank.'
-      });
-    }
-
-    email = email.toLowerCase();
-    email = email.trim();
-
-    User.find({
-      email: email
-    }, (err, users) => {
-      if (err) {
-        console.log('err 2:', err);
-        return res.send({
-          success: false,
-          message: 'Error: server error'
-        });
-      }
-      if (users.length != 1) {
-        return res.send({
-          success: false,
-          message: 'Error: Invalid'
-        });
-      }
-
-      const user = users[0];
-      if (!user.validPassword(password)) {
-        return res.send({
-          success: false,
-          message: 'Error: Invalid'
-        });
-      }
-
-      // Otherwise correct user
-      const userSession = new UserSession();
-      userSession.userId = user._id;
-      userSession.save((err, doc) => {
-        if (err) {
-          console.log(err);
-          return res.send({
-            success: false,
-            message: 'Error: server error'
-          });
-        }
-
-        return res.send({
-          success: true,
-          message: 'Valid sign in',
-          token: doc._id
-        });
-      });
-    });
-  });
-
-}
+  }); // end of sign up endpoint
+};
